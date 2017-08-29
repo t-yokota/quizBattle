f67de30666272bf7240957637837fffaa02e0f8b
Tomohiro <yokota.0529@gmail.com>
2017-08-30 02:50:28 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -5,3 +5,11 @@ var g=document.createElement("textarea");
 g.id = "answer";
 g.value = "test";
 document.getElementsByTagName( 'body' )[ 0 ].appendChild(g);
+
+2
+00:00:17,500 --> 00:00:19,460
+var str = document.getElementById('name').value;// || "太郎君";
+var synthes = new SpeechSynthesisUtterance(str);
+var voices =speechSynthesis.getVoices();
+voices.forEach(function(v, i){if(v.name == 'Google 日本語') synthes.voice = v;});
+speechSynthesis.speak(synthes);
