c2f327496fc04a86f297b15da8e0e8af535434c0
Tomohiro <yokota.0529@gmail.com>
2017-08-30 03:33:04 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,7 +1,5 @@
 1
 00:00:00,000 --> 00:00:03,460
-//text to speech example
-//loadScript('http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js');
 var g=document.createElement("textarea");
 g.id = "name";
 g.value = "次郎君";
