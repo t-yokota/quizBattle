c722be5d0d16a34785c2f2bb82e55422b74e055a
Tomohiro <yokota.0529@gmail.com>
2017-08-30 02:39:46 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,11 +1,15 @@
-00:00:00,000 --> 00:10:00,000
+00:00:00,000 --> 00:00;01,000
+var g=document.createElement("textarea");
+g.id = "answer";
+g.value = "test";
+document.getElementsByTagName( 'body' )[ 0 ].appendChild(g);
 var state = player.getPlayerState();
 if(state == -1){
-    未再生
+    
 }else if(state == 0){
-    終了
+    
 }else if(state == 1){
-    再生中
+    
 }else if(state == 2){
-    一時停止
+    
 }
