64f5d44e84c9dbe74dcc3462713acd04860026f5
Tomohiro <yokota.0529@gmail.com>
2017-08-30 02:48:56 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -5,13 +5,3 @@ var g=document.createElement("textarea");
 g.id = "answer";
 g.value = "test";
 document.getElementsByTagName( 'body' )[ 0 ].appendChild(g);
-var state = player.getPlayerState();
-if(state == -1){
-    
-}else if(state == 0){
-    
-}else if(state == 1){
-    
-}else if(state == 2){
-    
-}
