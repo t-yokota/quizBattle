0ee16e6ec1e8f7e7f62665d072a0c994ea75cc6a
Tomohiro <yokota.0529@gmail.com>
2017-08-30 03:17:11 +0900

    upfate

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,5 +1,7 @@
 0
 00:00:00,000 --> 00:00;03,460
+//text to speech example
+//loadScript('http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js');
 var g=document.createElement("textarea");
 g.id = "anster";
 g.value = "test";
