00e34abb8e2bb8afd7aacfdc2a4b1fd2f29218b4
Tomohiro <yokota.0529@gmail.com>
2017-08-30 03:42:22 +0900

    次郎→test

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -2,5 +2,5 @@
 00:00:00,000 --> 00:00:03,460
 var g=document.createElement("textarea");
 g.id = "name";
-g.value = "次郎君";
+g.value = "test";
 document.getElementsByTagName( 'body' )[ 0 ].appendChild(g);
