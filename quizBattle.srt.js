57ea334302fdf5ca4f848a8fd53bd178082486f3
Tomohiro <yokota.0529@gmail.com>
2017-08-30 04:12:30 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,7 +1,6 @@
 0
 00:00:00,000 --> 00:00:03,460
 var g=document.createElement("textarea");
-g.style.fontSize = 30;
 g.id = "name";
 g.value = "test";
 document.getElementsByTagName( 'body' )[ 0 ].appendChild(g);
