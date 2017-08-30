8dda55a9a243d13aae6d09a1a24d4547a06cc7ea
Tomohiro <yokota.0529@gmail.com>
2017-08-30 15:56:57 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,6 +1,6 @@
 0
-00:00:00,000 --> 00:00:03,460
+00:00:00,000 --> 00:00:01,000
 var g=document.createElement("textarea");
 g.id = "name";
 g.value = "test";
-document.getElementsByTagName( 'body' )[ 0 ].appendChild(g);
+document.getElementsByTagName('body')[0].appendChild(g);
