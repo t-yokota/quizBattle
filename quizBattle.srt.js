82f065a75e81bd3a99860c56831b3c685b7143df
Tomohiro <yokota.0529@gmail.com>
2017-09-03 05:25:30 +0900

    uprdate

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -2,7 +2,7 @@
 00:00:00,000 --> 00:00:01,000
 doOnce[index] = true;
 //var quesCount = documnt.createElement()
-var ansArea = document.createElement("h1");
+var ansArea = document.createElement("title");
 ansArea.id = "answer";
 ansArea.value = "write answer here.";
 document.getElementsByTagName('body')[0].appendChild(ansAea);
