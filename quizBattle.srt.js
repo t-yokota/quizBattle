98bdceb94ca1676933d554f645ecd9b2bb63a138
Tomohiro <yokota.0529@gmail.com>
2017-09-03 20:57:53 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -3,7 +3,7 @@
 doOnce[index] = true;
 var numQues = document.createElement("h1");
 var ansArea = document.createElement("textarea");
-cuntQues = "１";
+cuntQues = 1;
 var ques = document.createTextNode("第"+cuntQues+"問");
 numQues.appendChild(ques);
 ansArea.value = "Write answer here.";
@@ -12,4 +12,4 @@ document.getElementsByTagName("body")[0].appendChild(ansArea);
 
 0
 00:00:05,000 --> 00:00:06,000
-cuntQues = "2";
+cuntQues += 1;
