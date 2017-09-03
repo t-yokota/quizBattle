a993403156b33192f985b7b1475eb119aa65e201
Tomohiro <yokota.0529@gmail.com>
2017-09-04 03:54:32 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -14,14 +14,16 @@ ansBtn.id = "ansbtn";
 cntQues = 1;
 numQuesText = document.createTextNode("");
 numQues.appendChild(numQuesText);
-document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 // 解答欄
 ansCol.value = "ここに解答を記入して下さい";
 // 解答ボタン
 ansBtn.value = "解答を送信";
+//
 document.getElementsByTagName("body")[0].appendChild(numQues);
 document.getElementsByTagName("body")[0].appendChild(ansCol);
 document.getElementsByTagName("body")[0].appendChild(andBtn);
+//
+document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 
 2
 00:00:15,000 --> 00:00:16,000
