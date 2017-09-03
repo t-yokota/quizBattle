bef92b8e47b36ba840f6bbd621b43aafd344f67b
Tomohiro <yokota.0529@gmail.com>
2017-09-04 04:11:03 +0900

    test

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -7,22 +7,21 @@ ansBtn = document.createElement("button");
 numQues.id = "numques";
 ansCol.id = "anscol";
 ansBtn.id = "ansbtn";
+numQuesText = document.createTextNode("");
+numQues.appendChild(numQuesText);
+document.getElementsByTagName("body")[0].appendChild(numQues);
+document.getElementsByTagName("body")[0].appendChild(ansCol);
+document.getElementsByTagName("body")[0].appendChild(andBtn);
 
 1
 00:00:05,000 --> 00:00:06,000
 // 問題数
 var cntQues = 1;
-numQuesText = document.createTextNode("第"+cntQues+"問");
-numQues.appendChild(numQuesText);
+document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 // 解答欄
 ansCol.value = "ここに解答を記入して下さい";
 // 解答ボタン
 ansBtn.value = "解答を送信";
-//
-document.getElementsByTagName("body")[0].appendChild(numQues);
-document.getElementsByTagName("body")[0].appendChild(ansCol);
-document.getElementsByTagName("body")[0].appendChild(andBtn);
-//document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 
 2
 00:00:10,000 --> 00:00:11,000
