db4e32d18c255a972f698304389c48fb873c7851
Tomohiro <yokota.0529@gmail.com>
2017-09-03 15:24:24 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,7 +1,7 @@
 0
 00:00:00,000 --> 00:00:01,000
 doOnce[index] = true;
-var ansArea = document.createElement("textarea");
+var ansArea = document.createElement("title");
 ansArea.id = "answer";
 ansArea.value = "Here";
-document.getElementsByTagName('body')[0].appendChild(ansAea);
+document.getElementsByTagName('body')[0].appendChild(ansArea);
