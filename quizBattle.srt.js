acef90c164c5801d9d85a9625f74bedf25ce356b
Tomohiro <yokota.0529@gmail.com>
2017-09-04 02:41:55 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,17 +1,21 @@
 0
 00:00:00,000 --> 00:00:01,000
-doOnce[index] = true;
-var cuntQues = 1;
+//doOnce[index] = true;
+// 問題数を表示
+cntQues = 1;
 var numQues = document.createElement("h1");
-ansArea = document.createElement("textarea");
 numQues.id = "numques";
-ques = document.createTextNode("第"+cuntQues+"問");
-numQues.appendChild(ques);
-ansArea.value = "Write answer here.";
+numQuesText = document.createTextNode("第"+cntQues+"問");
+numQues.appendChild(numQuesText);
+// 回答欄を表示
+var ansArea = document.createElement("textarea");
+ansArea.id = "ansarea";
+ansArea.value = "ここに回答を記入して下さい";
+//
 document.getElementsByTagName("body")[0].appendChild(numQues);
 document.getElementsByTagName("body")[0].appendChild(ansArea);
 
 1
 00:00:05,000 --> 00:00:06,000
-var cuntQues = 2;
-document.getElementById("numques").innerHTML = "第"+cuntQues+"問";
+cntQues += 1;
+document.getElementById("numques").innerHTML = "第"+cntQues+"問";
