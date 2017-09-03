dbaf21e6515e7ee8aa1150d1ec471996ef188255
Tomohiro <yokota.0529@gmail.com>
2017-09-03 23:07:01 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,9 +1,10 @@
 0
 00:00:00,000 --> 00:00:01,000
 doOnce[index] = true;
-cuntQues = 1;
+var cuntQues = 1;
 numQues = document.createElement("h1");
 ansArea = document.createElement("textarea");
+numQues.id = "numques";
 ques = document.createTextNode("第"+cuntQues+"問");
 numQues.appendChild(ques);
 ansArea.value = "Write answer here.";
@@ -12,5 +13,5 @@ document.getElementsByTagName("body")[0].appendChild(ansArea);
 
 1
 00:00:05,000 --> 00:00:06,000
-cuntQues = 2;
+var cuntQues = 2;
 document.getElementById("numques").innerHTML = "第"+cuntQues+"問";
