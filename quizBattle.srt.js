6385be6123578de5b61c84b83de38f9c42cb5124
Tomohiro <yokota.0529@gmail.com>
2017-09-06 04:00:47 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -9,23 +9,22 @@ numQues.id = "numques";
 ansCol.id = "anscol";
 ansBtn.id = "ansbtn";
 numOX.id = "numox";
-QuizName = document.createTextNode();
-cntOX = document.createTextNode();
+QuizName = document.createTextNode("");
+cntOX = document.createTextNode("");
 numQues.appendChild(QuizName);
 numOX.appendChild(cntOX);
+document.getElementsByTagName("body")[0].appendChild(numQues);
+document.getElementsByTagName("body")[0].appendChild(ansCol);
+document.getElementsByTagName("body")[0].appendChild(ansBtn);
+document.getElementsByTagName("body")[0].appendChild(numOX);
 
 0
 00:00:00,200 --> 00:00:00,300
 document.getElementById("numques").innerHTML = "動画内の相手とクイズ対決";
 ansCol.value = "ここに解答を入力して下さい";
 ansBtn.innerHTML = "解答を送信";
-cntO = 0;
-cntX = 0;
+cntO = 0; cntX = 0;
 document.getElementById("numox").innerHTML = "◯: "+cntO+"\n✖: "+cntX;
-document.getElementsByTagName("body")[0].appendChild(numQues);
-document.getElementsByTagName("body")[0].appendChild(ansCol);
-document.getElementsByTagName("body")[0].appendChild(ansBtn);
-document.getElementsByTagName("body")[0].appendChild(numOX);
 
 0
 00:00:05,000 --> 00:00:06,000
