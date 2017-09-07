6f7befb1e532fda3378bb1cd4b40b4d5c8c551e0
Tomohiro <yokota.0529@gmail.com>
2017-09-07 16:23:13 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,17 +1,20 @@
 0
 00:00:00,000 --> 00:00:00,100
 doOnce[index] = true;
-numQues = document.createElement("h1"); //問題数を表示（はじめはクイズの企画名を表示）
+numQues = document.createElement("h1");      //問題数を表示（はじめはクイズの企画名を表示）
 ansCol = document.createElement("textarea"); //解答を入力するテキストエリア
-ansBtn = document.createElement("button"); //解答を送信するボタン
-numOX = document.createElement("h1"); //◯正解数と✖不正解数を表示
-sndPush = document.createElement("audio"); 
+ansBtn = document.createElement("button");   //解答を送信するボタン
+numOX = document.createElement("h1");        //◯正解数と✖不正解数を表示
+sndPush = document.createElement("audio");   //ボタンの押下音
+sndO = document.createElement("audio");      //正解音
+sndX = document.createElement("audio");      //不正解音
 numQues.id = "numques";
 ansCol.id = "anscol";
 ansBtn.id = "ansbtn";
 numOX.id = "numox";
-sndPush.id = "sndpush";
 sndPush.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/push.mp3";
+sndO.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/correct.mp3";
+sndX.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/discorrect.mp3";
 QuizName = document.createTextNode("");
 cntOX = document.createTextNode("");
 numQues.appendChild(QuizName);
@@ -21,21 +24,20 @@ document.getElementsByTagName("body")[0].appendChild(ansCol);
 document.getElementsByTagName("body")[0].appendChild(ansBtn);
 document.getElementsByTagName("body")[0].appendChild(numOX);
 document.onkeydown = pushButton1;
-function pushButton1(){if(event.keyCode == 32){sndPush.play(); player.pauseVideo();}}
 player.addEventListener('onStateChange', pushButton2);
+function pushButton1(){if(event.keyCode == 32){sndPush.play(); player.pauseVideo();}}
 function pushButton2(event){if(event.data == 2){sndPush.play();}}
 
 0
 00:00:00,200 --> 00:00:00,300
+cntO = 0; cntX = 0;
 document.getElementById("numques").innerHTML = "動画内の相手とクイズ対決";
 ansCol.value = "ここに解答を入力して下さい";
 ansBtn.innerHTML = "解答を送信";
-cntO = 0; cntX = 0;
-document.getElementById("numox").innerHTML = "◯: "+cntO+"  ✖: "+cntX;
+document.getElementById("numox").innerHTML = "◯: "+cntO+"\n✖: "+cntX;
 
 0
 00:00:05,000 --> 00:00:06,000
-sndPush.play();
 var cntQues = 1;
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 
