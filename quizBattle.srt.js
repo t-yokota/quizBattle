5d66be67b5d573025df0b8d5e07f9b10e5e86774
Tomohiro <yokota.0529@gmail.com>
2017-09-09 05:34:41 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -49,11 +49,12 @@ document.getElementById("numox").innerHTML = "◯: "+cntO+"  ✖: "+cntX;
 0
 00:00:05,000 --> 00:00:06,000
 var cntQues = 1;
+correctAns = correctAns1;
 ans1 = ansCol.value;
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 ansBtn.onclick = checkAnswer1;
 //function checkAnswer1(ans1, correctAns1){if(ans1.valueOf() === correctAns1){sndO.play();}}
-function checkAnswer1(ans1, correctAns1){document.getElementById("numques").innerHTML = ans1+", "+correctAns1;}
+function checkAnswer1(ans1, correctAns){document.getElementById("numques").innerHTML = ans1+", "+correctAns;}
 
 0
 00:00:10,000 --> 00:00:11,000
