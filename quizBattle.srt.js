a180ffd7b4acb0bf8b7874a66fa51731cb7942da
Tomohiro <yokota.0529@gmail.com>
2017-09-10 06:05:42 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -67,7 +67,7 @@ checkAnswer = function(correctAns, cntO, cntX){
     }
     document.getElementById("numox").innerHTML = "◯: "+cntO+"  ✖: "+cntX;    
     player.playVideo();
-    return (cntO, cntX);
+    return cntX;
 }
 
 0
@@ -87,8 +87,8 @@ ansBtn.onclick = function(){ window.setTimeout( function(){ cntOX = checkAnswer(
 0
 00:00:10,000 --> 00:00:11,000
 var cntQues = 2;
-cntO = cntOX[0];
-cntX = cntOX[1];
-document.getElementById("numox").innerHTML = "◯: "+cntOX[0]+"  ✖: "+cntOX[1];
+//cntO = cntOX[0];
+cntX = cntOX
+document.getElementById("numox").innerHTML = "◯: "+cntO+"  ✖: "+cntX;
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 ansBtn.onclick = function(){ window.setTimeout( function(){ cntOX = checkAnswer(correctAns2, cntO, cntX) }, 1000 ); };
