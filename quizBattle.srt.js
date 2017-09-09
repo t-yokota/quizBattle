3f038038182f011411bd8314325d69f414ff3ee4
Tomohiro <yokota.0529@gmail.com>
2017-09-09 17:09:33 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -37,7 +37,6 @@ function pushButton2(){if(player.getPlayerState() == 1){sndPush.play(); player.p
 //player.addEventListener('onStateChange', pushButton2);//カーソルのフォーカスがplayer内の場合キーイベントが呼べないため、onStageChange時に起動するイベントリスナー関数も用意する
 //function pushButton2(event){if(event.data == 2){sndPush.play();}}//スペースキーを押下->動画が停止(これをonStageChangeが取得)->音が鳴るという流れになるため、押下から音が鳴るまで若干遅延が生まれてしまう
 //
-function checkAnswer(correctAns){var ans = ansCol.value; if(ans.valueOf() === correctAns.valueOf()){sndO.play();}}
 correctAns1 = "ここに解答を入力して下さい";
 
 0
@@ -52,8 +51,8 @@ document.getElementById("numox").innerHTML = "◯: "+cntO+"  ✖: "+cntX;
 00:00:05,000 --> 00:00:06,000
 var cntQues = 1;
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
-ansBtn.onclick = function(){checkAnswer(correctAns1)};
-//function checkAnswer(correctAns){var ans = ansCol.value; document.getElementById("numques").innerHTML = ans+", "+correctAns;}
+ansBtn.onclick = window.setTimeout( function(){checkAnswer(correctAns1)}, 1000 );
+function checkAnswer(correctAns){var ans = ansCol.value; if(ans.valueOf() === correctAns.valueOf()){sndO.play();}}
 
 0
 00:00:10,000 --> 00:00:11,000
