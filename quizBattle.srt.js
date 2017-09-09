a97b9e4254a896aee90d6d85d603bbd11be51c4a
Tomohiro <yokota.0529@gmail.com>
2017-09-10 04:54:49 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -82,8 +82,7 @@ document.getElementById("numox").innerHTML = "◯: "+cntO+"  ✖: "+cntX;
 00:00:05,000 --> 00:00:06,000
 var cntQues = 1;
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
-ansBtn.onclick = function(){ window.setTimeout( function(){ checkAnswer(correctAns1, cntO, cntX) }, 1000 ); };
-(cntO, cntX) = ansBtn.onclick();
+ansBtn.onclick = function(){ window.setTimeout( (cnt0, cntX) = function(){ checkAnswer(correctAns1, cntO, cntX) }, 1000 ); };
 
 0
 00:00:10,000 --> 00:00:11,000
