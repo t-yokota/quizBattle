4361762408b24dc8f89c5e29f6073329a8e92153
Tomohiro <yokota.0529@gmail.com>
2017-09-10 17:43:55 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -83,13 +83,11 @@ var cntQues = 1;
 cntO = 0; cntX = 0;
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 document.getElementById("numox").innerHTML = "◯: "+cntO+" ✖: "+cntX;    
-ansBtn.onclick = function(){ window.setTimeout( function(){ cntOX = checkAnswer(correctAns1, cntO, cntX) }, 1000 ); };
+ansBtn.onclick = function(){ window.setTimeout( function(){ [cntO, cntX] = checkAnswer(correctAns1, cntO, cntX) }, 1000 ); };
 
 0
 00:00:10,000 --> 00:00:11,000
 var cntQues = 2;
-cntO = cntOX[0];
-cntX = cntOX[1];
 document.getElementById("numques").innerHTML = "第"+cntQues+"問";
 document.getElementById("numox").innerHTML = "◯: "+cntO+" ✖: "+cntX;    
-ansBtn.onclick = function(){ window.setTimeout( function(){ cntOX = checkAnswer(correctAns2, cntO, cntX) }, 1000 ); };
+ansBtn.onclick = function(){ window.setTimeout( function(){ [cntO, cntX] = checkAnswer(correctAns2, cntO, cntX) }, 1000 ); };
