c1db4c5c1dfd758a92d8a9ca860209c8b6241917
Tomohiro <yokota.0529@gmail.com>
2017-09-12 03:26:53 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -2,29 +2,23 @@
 00:00:00,000 --> 00:00:00,100
 doOnce[index] = true;
 numQues = document.createElement("h1");     //問題数を表示（はじめはクイズの企画名を表示）
+numOX = document.createElement("h1");       //◯正解数と✖不正解数を表示
 ansCol = document.createElement("textarea");//解答を入力するテキストエリア
 ansBtn = document.createElement("button");  //解答を送信するボタン
-numOX = document.createElement("h1");       //◯正解数と✖不正解数を表示
 sndPush = document.createElement("audio");  //ボタンの押下音
 sndO = document.createElement("audio");     //正解音
 sndX = document.createElement("audio");     //不正解音
 numQues.id = "numques";
+numOX.id = "numox";
 ansCol.id = "anscol";
 ansBtn.id = "ansbtn";
-numOX.id = "numox";
 sndPush.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/push.mp3";
 sndO.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/correct.mp3";
 sndX.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/discorrect.mp3";
-_numQues = document.createTextNode("");//書き換えが多いTextNodeの内容はdoOnce内のcreateElement(～appendChildまで)から隔離し、他のindex内でinnerHTMLを用いて編集する
+_numQues = document.createTextNode("");//書き換えが多いTextNodeの内容はdoOnce内から隔離し、他のindex内でinnerHTMLを用いて編集する
 _numOX = document.createTextNode("");
 numQues.appendChild(_numQues);
 numOX.appendChild(_numOX);
-document.getElementsByTagName("body")[0].appendChild(numQues);
-document.getElementsByTagName("body")[0].appendChild(ansCol);
-document.getElementsByTagName("body")[0].appendChild(ansBtn);
-document.getElementsByTagName("body")[0].appendChild(numOX);
-document.getElementById("ansbtn").focus();//再生直後にカーソルのフォーカスをjsの描画範囲(のボタンUI)に移動する->すぐにキーイベントが呼び出せるようになる
-document.getElementById("ansbtn").blur(); //ボタン自体にフォーカスをしている意味はないため、すぐにbulrでそれを解除
 player.addEventListener('onStateChange', focusJS);
 document.onkeydown = pushButton_keydown;
 document.onkeyup = pushButton_keyup;
@@ -87,6 +81,12 @@ checkAnswer = function(correctAns, cntO, cntX){
 
 0
 00:00:00,200 --> 00:00:00,300
+document.getElementsByTagName("body")[0].appendChild(numQues);
+document.getElementsByTagName("body")[0].appendChild(ansCol);
+document.getElementsByTagName("body")[0].appendChild(ansBtn);
+document.getElementsByTagName("body")[0].appendChild(numOX);
+document.getElementById("ansbtn").focus();//再生直後にカーソルのフォーカスをjsの描画範囲(のボタンUI)に移動する->すぐにキーイベントが呼び出せるようになる
+document.getElementById("ansbtn").blur(); //ボタン自体にフォーカスをしている意味はないため、すぐにbulrでそれを解除
 document.getElementById("numques").innerHTML = "クイズ対決";
 document.getElementById("numox").innerHTML = "◯: 0 ✖: 0";
 ansCol.value = "ここに解答を入力して下さい";
