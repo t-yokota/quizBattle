b836724e36669c1f32d6d185491cdd5f197b653b
Tomohiro <yokota.0529@gmail.com>
2017-09-18 01:28:07 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,36 +1,36 @@
 0
 00:00:00,000 --> 00:00:00,100
 doOnce[index] = true;
-numQues = document.createElement("h1");     //問題数を表示（はじめはクイズの企画名を表示）
+title = document.createElement("h1");       //動画のタイトルを表示
+subTex = document.createElement("h2");      //その他文章を表示
 numOX = document.createElement("h1");       //◯正解数と✖不正解数を表示
-text = document.createElement("h3");        //その他文章を表示
 ansCol = document.createElement("textarea");//解答を入力するテキストエリア
 ansBtn = document.createElement("button");  //解答を送信するボタン
 sndPush = document.createElement("audio");  //ボタンの押下音
 sndO = document.createElement("audio");     //正解音
 sndX = document.createElement("audio");     //不正解音
-numQues.id = "numques";
+title.id = "title";
+subTex.id = "subtex";
 numOX.id = "numox";
-text.id = "text";
 ansCol.id = "anscol";
 ansBtn.id = "ansbtn";
 sndPush.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/push.mp3";
 sndO.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/correct.mp3";
 sndX.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/discorrect.mp3";
-_numQues = document.createTextNode("");//書き換えが多いTextNodeの内容はdoOnce内から隔離し、他のindex内でinnerHTMLを用いて編集する
-_numOX = document.createTextNode("");
-_text = document.createTextNode("");
-numQues.appendChild(_numQues);
+_title = document.createTextNode("クイズ対決");
+_numOX = document.createTextNode("");//書き換えが多いTextNodeの内容はdoOnce内から隔離し、他のindex内でinnerHTMLを用いて編集する
+_subTex = document.createTextNode("");
+title.appendChild(_title);
 numOX.appendChild(_numOX);
-text.appendChild(_text);
-document.getElementsByTagName("body")[0].appendChild(numQues);
-document.getElementsByTagName("body")[0].appendChild(text);
+subTex.appendChild(_subTex);
+document.getElementsByTagName("body")[0].appendChild(title);
+document.getElementsByTagName("body")[0].appendChild(subTex);
 document.getElementsByTagName("body")[0].appendChild(ansCol);
 document.getElementsByTagName("body")[0].appendChild(ansBtn);
 document.getElementsByTagName("body")[0].appendChild(numOX);
 player.addEventListener('onStateChange', focusJS);
-//document.onkeydown = pushButton_keydown;
-//document.onkeyup = pushButton_keyup;
+document.onkeydown = pushButton_keydown;
+document.onkeyup = pushButton_keyup;
 document.ontouchstart = pushButton_touch;
 //解答の設定
 correctAns = [];
@@ -44,8 +44,7 @@ function focusJS(event){//再生開始後に必ずカーソルのフォーカス
         document.getElementById("ansbtn").blur();
     }
 }
-/*
-pushButton_keydown = function(){
+function pushButton_keydown(){
     if(event.keyCode == 32){
         if(player.getPlayerState() == 1){
             sndPush.play(); 
@@ -56,7 +55,7 @@ pushButton_keydown = function(){
         }
     }
 }
-pushButton_keyup = function(){
+function pushButton_keyup(){
     if(event.keyCode == 32){
         if(pushBool == 1){
             document.getElementById("anscol").focus();
@@ -65,7 +64,6 @@ pushButton_keyup = function(){
         }
     }
 }
-*/
 function pushButton_touch(){
     if(player.getPlayerState() == 1){
         sndPush.play();
@@ -90,81 +88,35 @@ checkAnswer = function(correctAns, cntO, cntX){
 
 0
 00:00:00,100 --> 00:00:00,200
-document.getElementById("numques").innerHTML = "クイズ対決";
+document.getElementById("title").innerHTML = "クイズ対決";
 ansCol.value = "ここに解答を入力して下さい";
 ansBtn.innerHTML = "解答を送信";
 
 0
 00:00:04,000 --> 00:00:04,100
 player.pauseVideo();
-document.getElementById("text").innerHTML = "スペースキーを押してボタンの動作を確認してください";
+document.getElementById("subTex").innerHTML = "スペースキーを押してボタンの動作を確認してください";
 document.getElementById("ansbtn").focus();//カーソルのフォーカスをjsの描画範囲(のボタンUI)に移動する->キーイベントが呼び出せるようになる
 document.getElementById("ansbtn").blur(); //ボタン自体にフォーカスをしている意味はないため、すぐにbulrでそれを解除
-//
-pushButton_keydown = function(){
-    if(event.keyCode == 32){
-        if(player.getPlayerState() == 2){
-            sndPush.play(); 
-            pushBool = 1;    
-        }
-    }
-}
-pushButton_keyup = function(){
-    if(event.keyCode == 32){
-        //func = function(){
-            if(pushBool == 1){
-                sndO.play(); 
-                player.playVideo();
-                pushBool = 0;  
-            }
-        //}
-        //setTimeout( function(){ func }, 1000 );
-    }
-}
-document.onkeydown = pushButton_keydown;
-//document.onkeyup = function(){ window.setTimeout( function(){ pushButton_keyup }, 1000); };
 
 0
 00:00:05,000 --> 00:00:06,000
 var cntQues = 1;
 cntO = 0; cntX = 0;
-document.getElementById("numques").innerHTML = "第"+cntQues+"問";
+document.getElementById("subTex").innerHTML = "第"+cntQues+"問";
 document.getElementById("numox").innerHTML = "◯: "+cntO+" ✖: "+cntX;    
 ansBtn.onclick = function(){ window.setTimeout( function(){ [cntO, cntX] = checkAnswer(correctAns[0], cntO, cntX) }, 1000 ); };
-//
-pushButton_keydown = function(){
-    if(event.keyCode == 32){
-        if(player.getPlayerState() == 1){
-            sndPush.play(); 
-            player.pauseVideo();
-            document.getElementById("anscol").focus();
-            ansCol.value = "";
-            pushBool = 1;    
-        }
-    }
-}
-pushButton_keyup = function(){
-    if(event.keyCode == 32){
-        if(pushBool == 1){
-            document.getElementById("anscol").focus();
-            ansCol.value = "";          
-            pushBool = 0;  
-        }
-    }
-}
-document.onkeydown = pushButton_keydown;
-document.onkeyup = pushButton_keyup;
 
 0
 00:00:10,000 --> 00:00:11,000
 var cntQues = 2;
-document.getElementById("numques").innerHTML = "第"+cntQues+"問";
+document.getElementById("subTex").innerHTML = "第"+cntQues+"問";
 document.getElementById("numox").innerHTML = "◯: "+cntO+" ✖: "+cntX;    
 ansBtn.onclick = function(){ window.setTimeout( function(){ [cntO, cntX] = checkAnswer(correctAns[1], cntO, cntX) }, 1000 ); };
 
 0
 00:00:15,000 --> 00:00:16,000
 var cntQues = 3;
-document.getElementById("numques").innerHTML = "第"+cntQues+"問";
+document.getElementById("subTex").innerHTML = "第"+cntQues+"問";
 document.getElementById("numox").innerHTML = "◯: "+cntO+" ✖: "+cntX;    
 ansBtn.onclick = function(){ window.setTimeout( function(){ [cntO, cntX] = checkAnswer(correctAns[2], cntO, cntX) }, 1000 ); };
