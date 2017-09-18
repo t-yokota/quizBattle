d5bece2ea18245d8ccf88396b15f1dcdad5273d7
Tomohiro <yokota.0529@gmail.com>
2017-09-19 01:52:56 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -47,8 +47,11 @@ function focusJS(event){//再生開始後に必ずカーソルのフォーカス
 function pushButton_keydown(){
     if(event.keyCode == 32){
         if(player.getPlayerState() == 2){
-            sndPush.play();
-            window.setTimeout( function(){ sndO.play(); player.playVideo(); }, 2000 );
+            //if(index == 2){
+                sndPush.play();
+                window.setTimeout( function(){ sndO.play() }, 2000 );
+                window.setTimeout( function(){ player.playVideo() }, 4000 );
+            //}
         }
         if(player.getPlayerState() == 1){
             sndPush.play();
