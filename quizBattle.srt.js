650976a17da380354368fa2974fed5e47a5bf9e1
Tomohiro <yokota.0529@gmail.com>
2018-08-11 17:17:26 +0900

    update

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -1,8 +1,6 @@
 0
 00:00:00,000 --> 00:00:00,100
 doOnce[index] = true;
-//
-//player.playerVars = { "autoplay": 1, "controls": 0 };
 //crateElement
 text    = document.createElement("h1");       //動画のタイトル等
 subText = document.createElement("h2");       //説明文等
@@ -25,8 +23,6 @@ sndPush.src = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/soun
 sndO.src    = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/correct.mp3";
 sndX.src    = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/sounds/discorrect.mp3";
 //正答リストの設定
-var fileName = "answer_UTF-8.csv";
-//var answerCSV = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/"+fileName;
 var answerCSV = "https://raw.githubusercontent.com/t-yokota/quizBattle/master/quizknock/geinoujinQuiz/answer_geinoujinQuiz.csv";
 file = new XMLHttpRequest();
 file.open("get", answerCSV, true);
@@ -39,7 +35,6 @@ function setAnswerArray(str){
         correctAnswer[i] = tmp[i].split(",");
     }
 }
-//キーイベント発生のためのイベントリスナーの設定
 //動画の再生・停止後に必ずjsの描画範囲内にフォーカスし、常にキーイベントが発生するようにする
 player.addEventListener('onStateChange', focusJS);
 function focusJS(){
@@ -58,33 +53,35 @@ buttonCheck = function(){
     }
 }
 //早押しのキーイベント関数１
-//問題中にスペースキー押下で動画を停止
-//問題ごとに押しの回数(cntPush)をカウントし、上限(limPush)を超えたら停止は不可に
+//問題中にスペースキーが押下されると動画を停止する
 var cntPush;
-var limPush     = 1;
+var limPush     = 50;
 var enablePush  = 1; //1のとき早押し可能
 var enableKeyup = 0; //1のときkeyupイベントが発生
 var enableCheck = 0; //1のとき答え合わせ可能
 pushButton_keydown = function(cntPush){
     if(event.keyCode == 32){
-        if(enablePush == 1){
-            if(limPush-cntPush > 0){
-                cntPush++;
-                sndPush.play();
-                enableKeyup = 1;
-                player.pauseVideo();
+        if(player.getPlayerState() == 1){
+            if(enablePush == 1){
+                //押しの回数(cntPush)をカウントし、上限(limPush)を超えたら停止は不可に
+                if(limPush-cntPush > 0){
+                    cntPush++;
+                    sndPush.play();
+                    enableKeyup = 1;
+                    player.pauseVideo();
+                }
             }
         }
     }
     return cntPush;
 }
 //早押しのキーイベント関数２
-//keyupの瞬間に解答欄にフォーカスし、解答の送信と答え合わせを可能にする(enableCheckを1に)
-//keydownのタイミングで解答欄にフォーカスすると、キーを離したときに解答欄に文字(スペース)を入力してしまう
+//keyupの瞬間に解答欄にフォーカス、解答の送信及び正誤判定を可能にする(enableCheckを1に)
+//(keydownのタイミングで解答欄にフォーカスすると、キーを離す瞬間に解答欄に文字(スペース)を入力してしまう)
 pushButton_keyup = function(){
     if(event.keyCode == 32){
         if(enableKeyup == 1){
-            subText.innerHTML = "解答は全角のひらがなと数字で入力してください。";
+            //subText.innerHTML = "解答は全角のひらがなと数字で入力してください。";
             ansCol.focus();
             ansCol.value = "";
             enablePush  = 0;
@@ -94,14 +91,14 @@ pushButton_keyup = function(){
     }
 }
 //正誤判定の関数
-//正答のcsvファイルはUTF-8でエンコードすること
+//(正答のcsvファイルはUTF-8でエンコードしておく)
 checkAnswer = function(correctAnswer, cntQues, cntPush, cntO, cntX){
     if(enableCheck == 1){
         var correctBool = 0;
         var ans = ansCol.value;
         var length = correctAnswer[cntQues-1].length;
         for(var i = 0; i < length; i++){
-            alert(correctAnswer[cntQues-1][i].valueOf());
+            //alert(correctAnswer[cntQues-1][i].valueOf());
             if(ans.valueOf() === correctAnswer[cntQues-1][i].valueOf()){
                 correctBool = 1;
             }
@@ -122,6 +119,53 @@ checkAnswer = function(correctAnswer, cntQues, cntPush, cntO, cntX){
     enableCheck = 0;
     player.playVideo();
     return [cntPush, cntO, cntX];
+}
+//解答入力の制限時間を設定
+inputTime = 10;//[sec]
+setInterval(timeLimit, 100);
+function timeLimit(){
+    //押しで動画が停止されたとき
+    if(enableCheck == 0){
+        elapsedTime = 0;
+    }else if(enableCheck == 1){
+        elapsedTime += 100;
+        subText.innerHTML = "あと"+(inputTime-Math.floor(elapsedTime/1000))+"秒で解答を送信してください";        
+        //subText.innerHTML = elapsedTime+", pushed";        
+        //解答の制限時間を超えたら動画を再生
+        if(Math.floor(elapsedTime) >= inputTime*1000){
+            player.playVideo();
+        }
+    }
+}
+//コントロールバーの使用制限
+//押し以外での動画の一時停止不可，シークバーによる再生位置のジャンプ不可
+var currentTime1 = 0;
+var currentTime2 = 0;
+setInterval(getTime1, 1000);
+player.addEventListener('onStateChange', getTime2);
+function getTime1(){
+    if(player.getPlayerState() == 1){
+        //時間取得（再生中） 
+        currentTime1 = player.getCurrentTime();
+        //subText.innerHTML = currentTime1+", "+currentTime2;
+    }
+}
+function getTime2(){
+    //押し以外で動画が停止された場合
+    if(player.getPlayerState() == 2 && enableKeyup == 0){
+        //時間取得（停止した瞬間）
+        currentTime2 = player.getCurrentTime();
+        //subText.innerHTML = currentTime1+", "+currentTime2;
+        //２つの時間が一致しない場合，シークバーによるジャンプとしてバーの位置を戻す
+        if(Math.floor(currentTime1) != Math.floor(currentTime2)){
+            player.seekTo(currentTime1);
+            currentTime2 = currentTime1;
+        }
+        //ボタンチェックの場合を除外して動画を再生
+        if(index != 2){
+            player.playVideo();
+        }
+    }
 }
 
 1
@@ -175,13 +219,16 @@ function whenNoSendAnswer(){
         [cntPush, cntO, cntX] = checkAnswer(correctAnswer, cntQues, cntPush, cntO, cntX);
     }
 }
-//解答送信ボタンは動画の停止中のみ有効にする
+//解答入力フォーム、解答送信ボタンは動画の停止中のみ有効にする
 player.addEventListener('onStateChange', ansBtnDisbled);
 function ansBtnDisbled(){
     if(player.getPlayerState() == 1){
         ansBtn.disabled = true;
+        ansCol.disabled = true;
+        ansCol.value = "ここに解答を入力";
     }else if(player.getPlayerState() == 2){
         ansBtn.disabled = false;
+        ansCol.disabled = false;
     }
 }
 
@@ -189,7 +236,7 @@ function ansBtnDisbled(){
 00:00:05,000 --> 00:00:06,000
 //doOnce[index] = true;
 //問題開始
-ansCol.disabled = false;
+//ansCol.disabled = false;
 cntO = 0;
 cntX = 0;
 //第１問
