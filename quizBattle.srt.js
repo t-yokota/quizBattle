1037953a972046196f12fd2351566df904306d85
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-03-22 13:51:16 +0900

    modified: quizBattle.srt.js, test of onerror function in image loading.

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -179,7 +179,7 @@ myApp.elem.pushBtn.onload = function(){
 //
 /* load image of push button */
 myApp.elem.imgBtn1.src = "https://github.com/t-yokota/quizBattle/raw/devel/convertToES6/figures/button_1.png";
-myApp.elem.imgBtn2.src = "";//"https://github.com/t-yokota/quizBattle/raw/devel/convertToES6/figures/button_2.png";
+myApp.elem.imgBtn2.src = "https://github.com/t-yokota/quizBattle/raw/devel/convertToES6/figures/button_2.png";
 myApp.elem.imgBtn3.src = "https://github.com/t-yokota/quizBattle/raw/devel/convertToES6/figures/button_3.png";
 //
 /* assign default image to push button */
