29c76535929df7094195639f7411aaa2443ff25b
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-04-19 16:45:04 +0900

    modified: quizBattle.srt.js, test of playsinline.

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -133,7 +133,7 @@ if (myApp.os != 'other'){
     myApp.elem.numOX.style.fontWeight = 'bold';
 }
 //
-player.setAttribute('playsinline', null);
+player.playsinline = true;
 //
 /* change player size */
 if(myApp.os != 'other'){
