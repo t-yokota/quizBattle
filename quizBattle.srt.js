56a22c67d48c32124bd8949719ca62f459632768
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-04-19 16:18:44 +0900

    modified: quizBattle.srt.js, typo fixed (before: playinline -> after: playsinline).

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -133,7 +133,7 @@ if (myApp.os != 'other'){
     myApp.elem.numOX.style.fontWeight = 'bold';
 }
 //
-player.setAttribule('playinline', '');
+player.setAttribule('playsinline', '');
 //
 /* change player size */
 if(myApp.os != 'other'){
