83151afd328e2253553b754a16ce43e1c8f1586d
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-04-30 14:41:02 +0900

    modified: quizBattle.srt.js, add event listener of resize push button when the orientation of smartphone is changed.

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -164,11 +164,7 @@ myApp.elem.pushBtn.onload = function(){
         alert("画像の読み込みに失敗しました。ページを再読み込みしてください。");
     }
 }
-document.addEventListener('orientationchange resize', function(){
-    if(window.innerWidth < window.innerHeight){
-        resizePushButton();
-    }
-})
+document.addEventListener('orientationchange resize', resizePushButton);
 function resizePushButton(){
     const tmpImgHeight = window.innerHeight-myApp.elem.pushBtn.getBoundingClientRect().top-parseInt(myApp.elem.numOX.style.lineHeight)-20;
     const tmpImgWidth  = myApp.elem.pushBtn.naturalWidth*tmpImgHeight/myApp.elem.pushBtn.naturalHeight;
