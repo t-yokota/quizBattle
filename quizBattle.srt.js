e6fb91ac562ffb7f994e1a61dc78e9ed1e968200
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-05-01 17:30:31 +0900

    modified: quizBattle.srt.js, add Math.floor() to the touch position value for debug.

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -526,7 +526,7 @@ function checkAnswer(){
 }
 //
 function printParams(){
-    myApp.elem.text.innerHTML = myApp.val.touchObject.pageX +','+ myApp.val.touchObject.pageY +': '+ myApp.val.pushBtnArea.top +', '+ myApp.val.pushBtnArea.bottom +', '+ myApp.val.pushBtnArea.righh +', '+ myApp.val.pushBtnArea.left;
+    myApp.elem.text.innerHTML = Math.floor(myApp.val.touchObject.pageX) +','+ Math.floor(myApp.val.touchObject.pageY) +': '+ Math.floor(myApp.val.pushBtnArea.top) +', '+ Math.floor(myApp.val.pushBtnArea.bottom) +', '+ Math.floor(myApp.val.pushBtnArea.right) +', '+ Math.floor(myApp.val.pushBtnArea.left);
     // myApp.elem.text.innerHTML = document.body.clientWidth / window.innerWidth;
     //myApp.elem.text.innerHTML = myApp.os + ', ' + navigator.userAgent;
     // myApp.elem.text.innerHTML = detectTouchPanel();
