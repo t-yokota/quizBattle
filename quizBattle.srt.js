118cf2c53c9ffeffee68237e82b66734a9a572ef
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-05-06 03:22:30 +0900

    modified: quizBattle.srt.js, test for fixing bug in previous commit.

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -600,12 +600,12 @@ function updatePushButtonArea(){
     myApp.val.pushBtnArea.top    = myApp.elem.pushBtn.getBoundingClientRect().top;
     myApp.val.pushBtnArea.bottom = myApp.elem.pushBtn.getBoundingClientRect().bottom;
     //
-    // if(myApp.val.os == 'iOS'){
-    //     myApp.val.pushBtnArea.left   = myApp.val.pushBtnArea.left   + window.pageXOffset;
-    //     myApp.val.pushBtnArea.right  = myApp.val.pushBtnArea.right  + window.pageXOffset;
-    //     myApp.val.pushBtnArea.top    = myApp.val.pushBtnArea.top    + window.pageYOffset;
-    //     myApp.val.pushBtnArea.bottom = myApp.val.pushBtnArea.bottom + window.pageYOffset;
-    // }
+    if(myApp.val.os == 'iOS'){
+        myApp.val.pushBtnArea.left   = myApp.val.pushBtnArea.left   + window.pageXOffset;
+        myApp.val.pushBtnArea.right  = myApp.val.pushBtnArea.right  + window.pageXOffset;
+        myApp.val.pushBtnArea.top    = myApp.val.pushBtnArea.top    + window.pageYOffset;
+        myApp.val.pushBtnArea.bottom = myApp.val.pushBtnArea.bottom + window.pageYOffset;
+    }
 }
 /**
  * focus to js element for an instant
