31de74f54554936277ab01902502ec9fffcbf359
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-05-16 18:54:08 +0900

    modified: quizBattle.srt.js, testing for playSilent().

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -624,7 +624,7 @@ function buttonCheck(responseInterval){
         myApp.elem.pushBtn.src = myApp.elem.imgBtn2.src;
         setTimeout(function(){ myApp.elem.pushBtn.src = myApp.elem.imgBtn3.src; }, 75);
     }
-    setTimeout(function(){ myApp.elem.sndO.mute(false); myApp.elem.sndO.play(); myApp.elem.pushBtn.src = myApp.elem.imgBtn1.src; }, responseInterval);
+    setTimeout(function(){ myApp.elem.sndO.play(); myApp.elem.pushBtn.src = myApp.elem.imgBtn1.src; }, responseInterval);
 }
 //
 function pushButton(){
