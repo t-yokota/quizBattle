d036e99cb8e0dc6c815327d6112845cc8e970998
Tomohiro Yokota <t-yokota@mbp-yokota.local>
2020-05-17 05:36:18 +0900

    modified: quizBattle.srt.js, test of audio sprite.

--- quizBattle.srt.js
+++ quizBattle.srt.js
@@ -737,7 +737,7 @@ function checkAnswer(){
 }
 //
 function printParams(){
-    myApp.elem.text.innerHTML = "sounds.currentTime: " + myApp.val.spriteData.pushBtn.end - myApp.elem.sounds.currentTime;
+    myApp.elem.text.innerHTML = "sounds.currentTime: " + myApp.val.spriteData.pushBtn.end/myApp.elem.sounds.currentTime;
     // myApp.elem.text.innerHTML = "docWidth: " + document.documentElement.clientWidth +', docHeight: '+ document.documentElement.clientHeight + ', inWidth: '+ window.innerWidth + ', inHeight: '+ window.innerHeight;
     // myApp.elem.text.innerHTML = "curr: " + myApp.elem.pushBtn.width +', new: '+ Math.floor(myApp.val.pushBtnWidth) + ', inWidth: '+ window.innerWidth + ', inHeight: '+ window.innerHeight;
     // myApp.elem.text.innerHTML = Math.floor(myApp.val.touchObject.pageX) +', '+ Math.floor(myApp.val.touchObject.pageY) +' ['+ Math.floor(myApp.val.pushBtnArea.left) +', '+ Math.floor(myApp.val.pushBtnArea.right) +'] ['+  Math.floor(myApp.val.pushBtnArea.top) +', '+ Math.floor(myApp.val.pushBtnArea.bottom)+'] | '+ window.pageXOffset + ', ' + window.pageYOffset;
