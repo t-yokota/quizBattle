2dd721376c3fcd88db1687ffb0e163bb7df0e996
Tomohiro Yokota <yokota.0529@gmail.com>
2020-07-26 08:31:30 +0900

    test of hidden check

--- contents/quizknock/geinouzinquiz/quizBattle.srt.js
+++ contents/quizknock/geinouzinquiz/quizBattle.srt.js
@@ -545,7 +545,7 @@ function myIntervalEvent(){
             myApp.val.currTime.playing = player.getCurrentTime();
             myApp.val.watchedTime = updateWatchedTime(myApp.val.currTime.playing, myApp.val.watchedTime);
             if(myApp.val.playingCount < 10){ myApp.val.playingCount += 1; }
-            if(myApp.val.currTime.playing -  myApp.val.watchedTime > 0.5 && myApp.val.playingCount >= 10){
+            if(myApp.val.currTime.playing -  myApp.val.watchedTime > 0.1 && myApp.val.playingCount >= 10){
                 alert('ページ内の処理が遅くなっています。早押しの判定に支障が出る可能性があるため、他のプロセスを終了してから改めてクイズをお楽しみください。');
             }
             /* prevent to play video before button check */
@@ -752,7 +752,7 @@ function instantFocusToElement(focusUsableElement){
 }
 //
 function updateWatchedTime(currentPlayingTime, watchedTime){
-    if(0.0 < currentPlayingTime - watchedTime && currentPlayingTime - watchedTime < 0.5){
+    if(0.0 < currentPlayingTime - watchedTime && currentPlayingTime - watchedTime < 0.1){
         watchedTime = currentPlayingTime;
     }
     return watchedTime;
