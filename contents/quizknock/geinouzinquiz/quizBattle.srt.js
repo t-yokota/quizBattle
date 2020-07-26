2f06afef370597d55e4c2c2b20eeb8fcbfb598aa
Tomohiro Yokota <yokota.0529@gmail.com>
2020-07-26 12:42:18 +0900

    test of hidden check

--- contents/quizknock/geinouzinquiz/quizBattle.srt.js
+++ contents/quizknock/geinouzinquiz/quizBattle.srt.js
@@ -545,7 +545,7 @@ function myIntervalEvent(){
             myApp.val.currTime.playing = player.getCurrentTime();
             myApp.val.watchedTime = updateWatchedTime(myApp.val.currTime.playing, myApp.val.watchedTime);
             if(myApp.val.playingCount < 10){ myApp.val.playingCount += 1; }
-            if(myApp.val.currTime.playing -  myApp.val.watchedTime > 0.1 && myApp.val.playingCount >= 10){
+            if(myApp.val.currTime.playing -  myApp.val.watchedTime > 1.0 && myApp.val.playingCount >= 10){
                 if(myApp.val.processDelayAlertBool == false){
                     alert('ページ内の処理が遅くなっています。早押しの判定に支障が出る可能性があるため、他のプロセスを終了してから改めてクイズをお楽しみください。このポップアップは一度のみ表示されます。');
                     myApp.val.processDelayAlertBool = true;
@@ -756,7 +756,7 @@ function instantFocusToElement(focusUsableElement){
 }
 //
 function updateWatchedTime(currentPlayingTime, watchedTime){
-    if(0.0 < currentPlayingTime - watchedTime && currentPlayingTime - watchedTime < 0.1){
+    if(0.0 < currentPlayingTime - watchedTime && currentPlayingTime - watchedTime < 1.0){
         watchedTime = currentPlayingTime;
     }
     return watchedTime;
@@ -874,6 +874,8 @@ function printParams(){
         "device: "           + myApp.val.os+"<br>"+
         "activeElem: "       + document.activeElement.id+"<br>"+   
         "status: "           + myApp.val.status+"<br>"+
+        "videoStatus: "      + player.getPlayerState()+"<br>"+
+        "playingCount: "     + myApp.val.playingCount+"<br>"+
         "timePlay: "         + myApp.val.currTime.playing.toFixed(3)+"<br>"+
         "timeStop: "         + myApp.val.currTime.stopped.toFixed(3)+"<br>"+
         "WatchedTime: "      + myApp.val.watchedTime.toFixed(3)+"<br>"+
