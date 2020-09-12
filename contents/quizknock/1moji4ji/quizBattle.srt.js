b6eeeab12503d8e607c563544eb288601a7db113
Tomohiro Yokota <yokota.0529@gmail.com>
2020-09-12 21:23:43 +0900

    tweaked resizePushButton() method for smartphone.

--- contents/quizknock/1moji4ji/quizBattle.srt.js
+++ contents/quizknock/1moji4ji/quizBattle.srt.js
@@ -761,7 +761,7 @@ function resizePushButton(){
     if(myApp.val.os != "other"){
         if(Math.abs(window.orientation) != 90){
             // const tmpImgHeight = document.documentElement.clientHeight-myApp.elem.pushBtn.getBoundingClientRect().top-parseInt(myApp.elem.numOX.style.lineHeight, 10);
-            const tmpImgHeight = document.documentElement.clientHeight-myApp.val.playerHeight-parseInt(myApp.elem.text.style.marginHeight, 10);//-parseInt(myApp.elem.ansCol.style.height, 10)-parseInt(myApp.elem.ansBtn.style.height, 10)-parseInt(myApp.elem.numOX.style.lineHeight, 10);
+            const tmpImgHeight = document.documentElement.clientHeight-myApp.val.playerHeight-32;//-parseInt(myApp.elem.ansCol.style.height, 10)-parseInt(myApp.elem.ansBtn.style.height, 10)-parseInt(myApp.elem.numOX.style.lineHeight, 10);
             myApp.elem.text.innerHTML = myApp.elem.text.style.margin;
             const tmpImgWidth  = myApp.elem.pushBtn.naturalWidth*tmpImgHeight/myApp.elem.pushBtn.naturalHeight;
             if(tmpImgWidth < document.documentElement.clientWidth){
